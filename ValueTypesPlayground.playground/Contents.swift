cde647f3d91b87476fcef1559258be6ebd639047
Danish Ahmed Ansari <mail.danishaa@gmail.com>
2015-09-30 19:21:02 +0530

    Changelog
    Value Types and Reference Types

--- ValueTypesPlayground.playground/Contents.swift
+++ ValueTypesPlayground.playground/Contents.swift
@@ -2,4 +2,42 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var a = 3
+var b = a
+
+b = 5
+
+a
+
+class number {
+    var n: Int
+    var m: Int
+    init(n: Int, m: Int) {
+        self.n = n
+        self.m = m
+    }
+}
+
+var aNumber = number(n: 3, m: 6)
+var bNumber = aNumber
+
+bNumber.n = 5
+aNumber.n
+
+bNumber.m = 10
+aNumber.m
+
+struct valueNumber {
+    var n: Int
+    init(n: Int) {
+        self.n = n
+    }
+}
+
+var aNum = valueNumber(n: 4)
+var bNum = aNum
+
+bNum.n = 10
+
+bNum.n
+aNum.n
